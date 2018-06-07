175604d53bd02286600696d0a8aad04df08c7cce
Welson Junior1 <wjunior1@Lenovo.com>
2018-06-07 09:35:39 -0300

    tests

--- src/app/service/charter.service.ts
+++ src/app/service/charter.service.ts
@@ -22,42 +22,91 @@ export class CharterService {
       width = w - margin.left - margin.right,
       height = h - margin.top - margin.bottom;
 
-    let svg = d3.select(element.nativeElement)
-      .append('svg')
-      .attr('width', width + margin.left + margin.right)
-      .attr('height', height + margin.top + margin.bottom)
-      .append('g')
-      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
     // Scale the range of the data
     const x = d3.scaleTime()
-        .domain(d3.extent(data, d => d.date))
-        .range([0, width]);
+    .domain(d3.extent(data, d => d.date))
+    .range([0, width]);
     const y = d3.scaleLinear()
-        .domain([0, d3.max(data, d => d.value)])
-        .range([height, 0]);
+    .domain([0, d3.max(data, d => d.value)])
+    .range([height, 0]);
 
+    let svg = d3.select(element.nativeElement).select('svg') ;
+    let g;
+    if (svg.empty()) {
+      svg = d3.select(element.nativeElement)
+        .append('svg')
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom);
 
-    // Add the X Axis
-    svg
-    .append('g')
+     g =  svg
+      .append('g')
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+        // Add the X Axis
+    g.append('g')
+    .attr('id', 'xaxis')
     .attr('transform', 'translate(0,' + height + ')')
     .call(d3.axisBottom(x));
 
     // Add the Y Axis
-    svg.append('g').call(d3.axisLeft(y));
+    g.append('g')
+    .attr('id', 'yaxis')
+    .call(d3.axisLeft(y));
 
-    svg.append('path')
-        .datum(data)
-        .attr('fill', 'none')
-        .attr('id', 'graphLine')
-        .attr('stroke', 'steelblue')
-        .attr('stroke-linejoin', 'round')
-        .attr('stroke-linecap', 'round')
-        .attr('stroke-width', 1.5)
-        .attr('d', d3.line<Data>()
-            .x(d => x(d.date))
-            .y(d => y(d.value))
-        );
+    } else {
+      g = svg.select('g');
+      d3.select('#xaxis').attr('transform', 'translate(0,' + height + ')').call(d3.axisBottom(x));
+      d3.select('#yaxis').call(d3.axisLeft(y));
+    }
+
+
+
+  
+
+    // d3.select('svg')
+    // .select('g')
+    // .append('path')
+    // .attr('id', 'graphLine');
+
+    let path = g.selectAll('#graphLine')
+                .data([data])
+                .attr('fill', 'none')
+                .attr('id', 'graphLine')
+                .attr('stroke', 'steelblue')
+                .attr('stroke-linejoin', 'round')
+                .attr('stroke-linecap', 'round')
+                .attr('stroke-width', 1.5)
+                .attr('d', d3.line<Data>()
+                    .x((d: Data) => x(d.date))
+                    .y((d: Data) => y(d.value)));
+
+      path.enter()
+          .append('path')
+          .attr('fill', 'none')
+          .attr('id', 'graphLine')
+          .attr('stroke', 'steelblue')
+          .attr('stroke-linejoin', 'round')
+          .attr('stroke-linecap', 'round')
+          .attr('stroke-width', 1.5)
+          .attr('d', d3.line<Data>()
+              .x((d: Data) => x(d.date))
+              .y((d: Data) => y(d.value))
+          )
+          .merge(path)
+              .attr('fill', 'none')
+              .attr('id', 'graphLine')
+              .attr('stroke', 'steelblue')
+              .attr('stroke-linejoin', 'round')
+              .attr('stroke-linecap', 'round')
+              .attr('stroke-width', 1.5)
+              .attr('d', d3.line<Data>()
+                  .x((d: Data) => x(d.date))
+                  .y((d: Data) => y(d.value)));
+
+          d3.select('svg').on('click', () => {
+            console.log(data[(data.push({date: new Date(), value: Math.random() * 1000 })) - 1]);
+            this.drawChart(data, element);
+          });
   }
 }
